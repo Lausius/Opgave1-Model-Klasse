6fc50e1202ffb1737a5bb3cb43b70800eebab80c
Lausius <skinkens_email@yahoo.dk>
2020-09-21 17:22:04 +0200

    Add project files.

---
+++ Model-Klasse-Test/FanOutputTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave1_Model_Klasse;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model_Klasse_Test
+{
+    [TestClass]
+    public class FanOutputTests
+    {
+        FanOutput fanOutput = new FanOutput();
+        [TestMethod]
+        public void AtLeastTwoCharactersInNameTest()
+        {
+            Assert.AreEqual("Karl", fanOutput.Name = "Karl");
+            try
+            {
+                fanOutput.Name = "S";
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.AreEqual("Navn skal mindst indeholde 2 eller flere bogstaver (Parameter 'Navn')", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void BoundaryValuesOfTemperatureTest()
+        {
+            Assert.AreEqual(15, fanOutput.Temperature = 15);
+            Assert.AreEqual(25, fanOutput.Temperature = 25);
+            try
+            {
+                fanOutput.Temperature = 14;
+                Assert.Fail();
+                fanOutput.Temperature = 26;
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.AreEqual("Temperaturen skal ligge mellem 15 og 25 grader celsius (Parameter 'Temperatur')", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void BoundaryValuesOfHumidity()
+        {
+            Assert.AreEqual(30, fanOutput.Humidity = 30);
+            Assert.AreEqual(80, fanOutput.Humidity = 80);
+            try
+            {
+                fanOutput.Humidity = 29;
+                Assert.Fail();
+                fanOutput.Humidity = 81;
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.AreEqual("Fugtigheden skal have en værdi mellem 30 og 80 (Parameter 'Fugtighed')", ex.Message);
+            }
+        }
+    }
+}
---
+++ Opgave1-Model-Klasse/FanOutput.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Opgave1_Model_Klasse
+{
+    public class FanOutput
+    {
+        private int _id;
+        private string _name;
+        private double _temperature;
+        private double _humidity;
+
+        public int Id 
+        { 
+            get { return _id; }
+            set { _id = value; } 
+        }
+        public string Name 
+        { 
+            get { return _name; }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Navn", "Navn skal mindst indeholde 2 eller flere bogstaver");
+                }
+            }
+        }
+        public double Temperature 
+        { 
+            get { return _temperature; }
+            set
+            {
+                if (value >= 15 && value <= 25)
+                {
+                    _temperature = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Temperatur", "Temperaturen skal ligge mellem 15 og 25 grader celsius");
+                }
+            }
+        }
+        public double Humidity 
+        { 
+            get { return _humidity; }
+            set
+            {
+                if (value >= 30 && value <= 80)
+                {
+                    _temperature = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Fugtighed", "Fugtigheden skal have en værdi mellem 30 og 80");
+                }
+            }
+        }
+
+    }
+}
